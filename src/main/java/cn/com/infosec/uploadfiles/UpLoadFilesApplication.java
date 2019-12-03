6913d28f382b7df7ddb68d0f05aebd43c5042b25
lijk8090 <lijk8090@gmail.com>
2019-12-03 18:56:27 +0800

    svn: 2011

---
+++ src/main/java/cn/com/infosec/uploadfiles/UpLoadFilesApplication.java
@@ -0,0 +1,13 @@
+package cn.com.infosec.uploadfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpLoadFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpLoadFilesApplication.class, args);
+	}
+
+}
